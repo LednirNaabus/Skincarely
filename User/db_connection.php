61cd6cb195ae27d75dfc736eebbba7600f1039cf
Rindel Subaan <valsubaan@yahoo.com>
2022-03-30 20:43:42 +0800

    database connection

--- User/db_connection.php
+++ User/db_connection.php
@@ -1,33 +1,10 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pw = '';
-$db = "skincarely";
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'skincarely');
 
-$con = mysqli_connect($host,$user,$pw,$db);
-if(mysqli_connect_errno()) {
-    die("Failed to connect with MySQL: ".mysqli_connect_error());
-}
-
-$f_name = $_POST['fn'];
-$l_name = $_POST['ln'];
-$user_name = $_POST['username'];
-$email = $_POST['email'];
-$pass = $_POST['pass'];
-$name = $f_name . " " . $l_name;
-$created = date("Y-m-d");
-
-$sql = "INSERT INTO customers (customer_username, customer_password, customer_name, customer_email, customer_created) VALUES ('$user_name', '$pass', '$name', '$email', '$created')";
-
-if(mysqli_query($con, $sql)) {
-    ob_start();
-    header("Location: login_page.php");
-    ob_end_flush();
-} else {
-    echo "Error: " .$sql. "<br>" .mysqli_error($con);
-}
-
-mysqli_close($con);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 ?>
--- User/login_page.php
+++ User/login_page.php
@@ -16,7 +16,7 @@
             </div>
             <div class="col-md-6 col-lg-7 d-flex align-items-center">
               <div class="card-body p-4 p-lg-5 text-black">
-                <form action="#" method="post" class="needs-validation" novalidate>
+                <form action="" method="post" class="needs-validation" novalidate>
                   <div class="d-flex align-items-center mb-3 pb-1">
                   <i class="fas fa-crow fa-2x me-3" style="color: #AC7672;"></i>
                     <span class="h1 fw-bold mb-0">Skincarely</span>
