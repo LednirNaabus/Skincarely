9b20d8740b153af26c5c92db2715e70cd29e873f
Rindel Subaan <valsubaan@yahoo.com>
2022-03-30 17:54:55 +0800

    navbar for landing page

---
+++ User/includes/landing/navbar.php
@@ -0,0 +1,4 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a href="#" class="navbar-brand">Skincarely</a>
+    <button class="navbar-toggler" type="button"></button>
+</nav>
