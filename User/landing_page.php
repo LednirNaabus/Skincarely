cb155de5c51a5a19e37daa5b7590bde7d4068ad4
Rindel Subaan <valsubaan@yahoo.com>
2022-03-31 22:24:19 +0800

    added boiler template for landing page

--- User/landing_page.php
+++ User/landing_page.php
@@ -1,7 +1,19 @@
+<?php 
+    session_start();
+
+    // if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    //     header("location: login.php");
+    //     exit;
+    // }
+?>
+
+
 <?php
     include 'includes/landing/head.php';
 ?>
-
+<?php
+echo "Hi " . htmlspecialchars($_SESSION["username"]);
+?>
 <?php
     include 'includes/landing/footer.php';
 ?>
