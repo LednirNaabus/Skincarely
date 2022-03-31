52baf673f64a486e2d3304fe81a0a140c0b7e46f
Rindel Subaan <valsubaan@yahoo.com>
2022-03-31 15:33:00 +0800

    email, username, and password validation for registration

--- User/registration.php
+++ User/registration.php
@@ -1,6 +1,79 @@
 <?php
-    include 'includes/login/head.php';
+  require_once "db_connection.php";
 
+  $username = $password = $confirm_password = $first_name = $last_name = $email = "";
+  $username_err = $password_err = $confirm_password_err = $first_name_err = $last_name_err = $email_err = "";
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    //validate username
+    if(empty(trim($_POST['username']))) {
+      $username_err = "Please enter a valid username.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST['username']))) {
+      $username_err = "Username should contain numbers and letters.";
+    } else {
+      // prepare select statement
+      $sql = "SELECT customer_id FROM customers WHERE customer_username = ?";
+
+      if($stmt = mysqli_prepare($link, $sql)) {
+        mysqli_stmt_bind_param($stmt, "s", $param_username);
+        $param_username = trim($_POST['username']);
+        if(mysqli_stmt_execute($stmt)) {
+          mysqli_stmt_store_result($stmt);
+          if(mysqli_stmt_num_rows($stmt) == 1) {
+            $username_err = "Username already taken.";
+          } else {
+            $username = trim($_POST['username']);
+          }
+        } else {
+          echo "Error.";
+        }
+        mysqli_stmt_close($stmt);
+      }
+    }
+
+    //validate email
+    if(empty(trim($_POST['email']))) {
+      $email_err = "Please enter your email.";
+    } else {
+      $sql = "SELECT customer_id FROM customers where customer_email = ?";
+      
+      if($stmt = mysqli_prepare($link, $sql)) {
+        mysqli_stmt_bind_param($stmt, "s", $param_email);
+        $param_email = trim($_POST['email']);
+        if(mysqli_stmt_execute($stmt)) {
+          mysqli_stmt_store_result($stmt);
+          if(mysqli_stmt_num_rows($stmt) == 1) {
+            $email_err = "Email already taken.";
+          } else {
+            $email = trim($_POST['email']);
+          }
+        } else {
+          echo "Error.";
+        }
+        mysqli_stmt_close($stmt);
+      }
+    }
+
+    //validate password and password confirmation
+    if(empty(trim($_POST['password']))) {
+      $password_err = "Please enter your password.";
+    } elseif(strlen(trim($_POST['password']) < 7)) {
+      $password_err = "Password must be atleast 7 characters.";
+    } else {
+      $password = trim($_POST['password']);
+    }
+
+    if(empty(trim($_POST['confirm_password']))) {
+      $confirm_password_err = "Please confirm your password.";
+    } else {
+      $confirm_password = trim($_POST['confirm_password']);
+      if(empty($password_err) && ($password != $confirm_password)) {
+        $confirm_password_err = "Password confirmation did not match.";
+      }
+    }
+
+  } //end if
 ?>
 <section class="vh-100" style="background-color: #f7f3f2;">
   <div class="container py-5 h-100">
@@ -17,7 +90,7 @@
             </div>
             <div class="col-md-6 col-lg-7 d-flex align-items-center">
               <div class="card-body p-4 p-lg-5 text-black">
-                <form action="db_connection.php" method="post" enctype="multipart/form-data" class="needs-validation" novalidate>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
                   <div class="d-flex align-items-center mb-4 pb-3">
                   <i class="fas fa-crow fa-2x me-3" style="color: #AC7672;"></i>
                     <span class="h1 fw-bold mb-0">Skincarely</span>
@@ -29,7 +102,7 @@
                       <div class="col-md-6 mb-4">
                           <div class="form-outline">
                             <label for="fn" class="form-label">First Name</label>
-                            <input type="text" name="fn" class="form-control" required/>
+                            <input type="text" name="fn" class="form-control"/>
                             <span class="invalid-feedback">
                                 Please enter your first name.
                             </span>
@@ -38,7 +111,7 @@
                       <div class="col-md-6 mb-4">
                           <div class="form-outline">
                             <label for="ln" class="form-label">Last Name</label>
-                            <input type="text" name="ln" class="form-control" required/>
+                            <input type="text" name="ln" class="form-control"/>
                             <span class="invalid-feedback">
                                 Please enter your last name.
                             </span>
@@ -50,18 +123,18 @@
                       <div class="col-md-6 mb-4">
                           <div class="form-outline">
                               <label for="username" class="form-label">Username</label>
-                              <input type="text" name="username" class="form-control" required/>
+                              <input type="text" name="username" class="form-control <?php echo(!empty($username_err)) ? 'is-invalid' : ''; ?>"/>
                               <span class="invalid-feedback">
-                                  Please enter a valid username.
+                                  <?php echo $username_err; ?>
                               </span>
                           </div>
                       </div>
                       <div class="col-md-6 mb-4">
                           <div class="form-outline">
-                              <label for="email" class="form-label">Email</label>
-                              <input type="email" name="email" class="form-control" required/>
+                              <label for="email" class="form-label <?php echo(!empty($email_err)) ? 'is-invalid' : '';?>">Email</label>
+                              <input type="email" name="email" class="form-control"/>
                               <span class="invalid-feedback">
-                                  Please enter your email.
+                                  <?php echo $email_err; ?>
                               </span>
                           </div>
                       </div>
@@ -70,9 +143,9 @@
                   <div class="row">
                       <div class="form-outline mb-4">
                           <label for="password" class="form-label">Password</label>
-                          <input type="password" name="password" class="form-control" required/>
+                          <input type="password" name="password" class="form-control <?php echo(!empty($password_err)) ? 'is-invalid' : ''; ?>"/>
                           <span class="invalid-feedback">
-                              Please enter your password.
+                              <?php echo $password_err; ?>
                           </span>
                       </div>
                   </div>
@@ -92,6 +165,10 @@
     </div>
   </div>
 </section>
+<?php
+    include 'includes/login/head.php';
+?>
+
 <?php
     include 'includes/login/footer.php';
 ?>
