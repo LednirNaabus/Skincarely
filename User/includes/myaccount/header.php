48e0bfb82a1439462ab907ae3bdbd499471b1c80
Rindel Subaan <valsubaan@yahoo.com>
2022-04-05 21:15:23 +0800

    added to includes

--- User/includes/myaccount/header.php
+++ User/includes/myaccount/header.php
@@ -1,45 +1,49 @@
-<!DOCTYPE html>
+ 
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>
-  EFF ADMIN 
+  Skincarely
   </title> 
   <link rel = "icon" href = 
   "https://i.ibb.co/mJrCGQW/wrench.png" 
   type = "image/x-icon">    
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
   <link href='https://fonts.googleapis.com/css?family=Calistoga' rel='stylesheet'>
   <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" >
-  
   <!-- Font Awesome --> 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> 
   <!-- Main style -->
-  <link rel="stylesheet" href="dist/css/main.css"> 
+  <link rel="stylesheet" type="text/css"href="dist/css/main.css?version=1"> 
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css"> 
-</head> 
-
+</head>  
 <body>
 <div class="wrapper">  
   <!-- Navbar -->
-  <nav class="main-header navbar   navbar-white navbar-light"> 
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-      </li> 
-    </ul> 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">  
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li> 
-    </ul>
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light"> 
+  <ul class="navbar-nav"> 
+    <li class="nav-item d-none d-sm-inline-block"> 
+    <div class="d-flex align-items-center ">
+    <i class="fas fa-crow fa-2x me-3" style="color: #AC7672; padding: 0 1rem 0;" aria-hidden="true"></i>
+      <span class="h2 mb-0">Skincarely</span>
+    </div> 
+    </li> 
+  </ul> 
+  <ul class="navbar-nav ml-auto">  
+  <li class="nav-item">
+    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+      <i class="fas fa-expand-arrows-alt" style="color: #AC7672;" aria-hidden="true"></i>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+      <i class="fa-solid fa-user" style="color: #AC7672;" aria-hidden="true"></i>
+    </a>
+  </li>
+  </ul>
   </nav>
-  <!-- /.navbar -->   
+    <!-- /.navbar -->   
