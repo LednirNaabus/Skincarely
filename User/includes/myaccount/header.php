ec21c2b98b73f065efbbbdb8d43538774b5e102d
Rindel Subaan <valsubaan@yahoo.com>
2022-04-05 18:30:41 +0800

    includes for myaccount page

---
+++ User/includes/myaccount/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>
+  EFF ADMIN 
+  </title> 
+  <link rel = "icon" href = 
+  "https://i.ibb.co/mJrCGQW/wrench.png" 
+  type = "image/x-icon">    
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <link href='https://fonts.googleapis.com/css?family=Calistoga' rel='stylesheet'>
+  <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" >
+  
+  <!-- Font Awesome --> 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> 
+  <!-- Main style -->
+  <link rel="stylesheet" href="dist/css/main.css"> 
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css"> 
+</head> 
+
+<body>
+<div class="wrapper">  
+  <!-- Navbar -->
+  <nav class="main-header navbar   navbar-white navbar-light"> 
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+      </li> 
+    </ul> 
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">  
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li> 
+    </ul>
+  </nav>
+  <!-- /.navbar -->   
