3441b581d361d0aa58dc3fc638f089308e67caa0
Rindel Subaan <valsubaan@yahoo.com>
2022-04-06 20:10:32 +0800

    changed footer class added bootstrap src in header include

--- User/includes/main/footer.php
+++ User/includes/main/footer.php
@@ -1,6 +1,5 @@
-<footer class="main-footer">
-  <strong>Copyright Skincarely &copy; 2021 <a href=" "></a>.</strong>
-  All rights reserved.
+<footer class="container text-center">
+<strong>Skincarely.ph</strong> &copy; 2021. All rights reserved.
   <div class="float-right d-none d-sm-inline-block">
     <b>Version</b> 1.0
   </div>
--- User/includes/main/header.php
+++ User/includes/main/header.php
@@ -4,7 +4,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>
   Skincarely
-  </title>    
+  </title> 
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="dist/css/bootstrap.min.css">   
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <!-- Sripts -->
@@ -24,30 +26,31 @@
   <!-- Font awesome icons -->
   <script src="https://kit.fontawesome.com/d9fb1896a3.js" crossorigin="anonymous"></script>  
   <style>
-        /* Hours wasted on styling the jumbotron: 2139218312 hours
-        I hate CSS */
-        .bg-img {
-            background-image: url('dist/img/landing-cover.jpg');
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: cover;
-            z-index: -1;
-        }
+  /* Hours wasted on styling the jumbotron: 2139218312 hours
+  I hate CSS */
+  .bg-img {
+      background-image: url('dist/img/landing-cover.jpg');
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      z-index: -1;
+  }
 
-        .bg-img .jumbotron {
-            background: linear-gradient(0deg, #00000088 30%, #FFFFFF44 100%);
-            color: #fff;
-            z-index: 0;
-        }
-    </style>
+  .bg-img .jumbotron {
+      background: linear-gradient(0deg, #00000088 30%, #FFFFFF44 100%);
+      color: #fff;
+      z-index: 0;
+  }
+  </style>
 </head>  
-<body style="padding-top: 3.5rem;">
-<div class="wrapper"> 
+<body>
 <?php  
     session_start();
     if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
         header("location: login_page.php");
         exit;
     }
-    include 'includes/main/navbar.php'; 
 ?>
+<header>
+  <?php include 'includes/main/navbar.php'; ?>
+</header>
