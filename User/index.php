5d485990895c19725de66aadbda23d64087531dc
Rindel Subaan <valsubaan@yahoo.com>
2022-04-06 20:10:47 +0800

    btn class in index changed

--- User/index.php
+++ User/index.php
@@ -19,17 +19,17 @@
             <div class="col-md-4">
                 <h2>Heading</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, alias eos accusamus odio, fugiat consequuntur dignissimos impedit, sunt et nostrum facilis inventore. Similique impedit soluta quas voluptatem incidunt quos voluptates.</p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2>Heading</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, alias eos accusamus odio, fugiat consequuntur dignissimos impedit, sunt et nostrum facilis inventore. Similique impedit soluta quas voluptatem incidunt quos voluptates.</p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2>Heading</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam harum reprehenderit earum perspiciatis error minus fuga ducimus quod! Dignissimos inventore perferendis odio soluta autem ullam eligendi excepturi architecto neque itaque!</p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
             </div>
         </div>
         <hr>
