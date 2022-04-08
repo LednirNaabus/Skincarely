4ef6258c91b35b705a4772dfaecd98c407c9d605
Rindel Subaan <valsubaan@yahoo.com>
2022-04-09 01:47:31 +0800

    feature: landing page fetch shop names for featured shops

--- User/index.php
+++ User/index.php
@@ -1,5 +1,8 @@
 <?php
     include 'includes/main/header.php';
+    include 'includes/connection/db_connection.php';
+
+    $result = mysqli_query($link, "SELECT * FROM shops");
 ?> 
 <main role="main" style="background-color: #F7F3F2;">
     <div class="bg-img">
@@ -17,20 +20,15 @@
     <!-- featured vendors/items/shops here -->
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <h2>Heading</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, alias eos accusamus odio, fugiat consequuntur dignissimos impedit, sunt et nostrum facilis inventore. Similique impedit soluta quas voluptatem incidunt quos voluptates.</p>
-                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-            </div>
-            <div class="col-md-4">
-                <h2>Heading</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, alias eos accusamus odio, fugiat consequuntur dignissimos impedit, sunt et nostrum facilis inventore. Similique impedit soluta quas voluptatem incidunt quos voluptates.</p>
-                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-            </div>
-            <div class="col-md-4">
-                <h2>Heading</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam harum reprehenderit earum perspiciatis error minus fuga ducimus quod! Dignissimos inventore perferendis odio soluta autem ullam eligendi excepturi architecto neque itaque!</p>
-                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+            <h1 class="display-4">Discover products you'll love</h1>
+            <p>Browse and search for a specific make up or lipstick you like at exclusive prices.</p>
+        </div>
+        <div class="row">
+            <div class="input-group mb-3">
+                <input type="text" placeholder="Search..." class="form-control" aria-label="Search">
+                <span class="input-group-append">
+                        <button class="btn btn-primary" type="button">Search</button>
+                </span>
             </div>
         </div>
         <hr>
@@ -38,11 +36,31 @@
         <div class="row">
             <div class="container">
                 <div class="col-md-20">
-                    <h2>Popular near you</h2>
+                    <!-- for now, will list first few shops sa shops table -->
+                    <h2>Featured Shops</h2>
                     <div class="row">
-                        <div class="col-md-4">test</div>
-                        <div class="col-md-4">test</div>
-                        <div class="col-md-4">test</div>
+                        <?php
+                            while($row = mysqli_fetch_array($result)) {
+                                if($row['shop_id'] < 6) {
+                                    echo '<div class="col-md-4">';
+                                    echo '<p class="lead">'.$row['shop_name'].'</p>';
+                                    echo '<p><a class="btn btn-primary" href="#" role="button">View</a></p>';
+                                    echo '</div>';
+                                }
+                            }
+                        ?>
+                    </div>
+                    <h2>Promoted</h2>
+                    <div class="row">
+                        
+                    </div>
+                    <h2>Recommended</h2>
+                    <div class="row">
+                        
+                    </div>
+                    <h2>New Shops to check out!</h2>
+                    <div class="row">
+                        
                     </div>
                 </div>
             </div>
