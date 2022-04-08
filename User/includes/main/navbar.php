9ea2b1391cc6d1c440a5ce7b0e221803bdff709f
Kobe Sergio <ckasergio@gmail.com>
2022-04-08 14:35:01 +0800

    changes

--- User/includes/main/navbar.php
+++ User/includes/main/navbar.php
@@ -1,26 +1,26 @@
-<!-- Navbar -->
-<nav class="navbar navbar-expand-md fixed-top navbar-light" style="background-color: #AC7672;">
-    <div class="d-flex align-items-center">
-        <i class="fas fa-crow fa-2x me-3" style="padding: 0 1rem 0;"></i>
-        <a href="index.php" class="navbar-brand" style="color: #fff;">Skincarely</a>
-    </div>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon">
-        </span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a href="MyAccount.php" class="nav-link">
-                    <i class="fa-solid fa-user" aria-hidden="true" style="padding: 10px;"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-widget="fullscreen" role="button">
-                    <i class="fa-solid fa-arrows-maximize" aria-hidden="true" style="padding: 10px"></i>
-                </a>
-            </li>
-        </ul>
-    </div>
-</nav>
-<!-- /Navbar -->
+<header>
+    <nav class="navbar navbar-expand-md fixed-top navbar-light" style="background-color: #AC7672;">
+        <div class="d-flex align-items-center">
+            <i class="fas fa-crow fa-2x me-3" style="padding: 0 1rem 0;"></i>
+            <a href="index.php" class="navbar-brand" style="color: #fff;">Skincarely</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+            </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="MyAccount.php" class="nav-link">
+                        <i class="fa-solid fa-user" aria-hidden="true" style="padding: 10px;"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="fullscreen" role="button">
+                        <i class="fa-solid fa-arrows-maximize" aria-hidden="true" style="padding: 10px"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav> 
+</header>
