a85c145d6501a5235b60b352a1be2983dfbf2c80
Ayesha Keith Santos <ayeshakeithsantos@gmail.com>
2022-04-08 22:34:16 +0800

    wip products (vendor-side)
    - renamed skincarely to index
    - add product working and linked to db (but i still need to fix the checkbox part, currently reads on or off only)
    - shows shop products also by gridview (still have to work on the filter part)

--- Admin/shop_creation.php
+++ Admin/shop_creation.php
@@ -32,9 +32,23 @@ include('connection.php');
             $sql = "INSERT INTO shops (vendor_id, shop_name, shop_description, shop_logo, shop_location, shop_socials, shop_contact) VALUES ('$linkvs', '$shopname', '$shopdescription', '$shoplogo', '$mainbranch', '$socials', '$contact')";
 
               if (mysqli_query($con, $sql)) {
+
+                $sqlname = "SELECT * FROM shops WHERE vendor_id = '$linkvs';";
+                $resultname = mysqli_query($con, $sqlname);
+
+                if (mysqli_num_rows($resultname) > 0) {
+                    while($row = mysqli_fetch_assoc($resultname)) {
+                        $_SESSION["shopid"] = $row["shop_id"];
+                    }
+
                   ob_start();
                   header("Location: merchant_shopprofile.php");
                   ob_end_flush(); 
+                } else {
+                    echo "0 results";
+                }
+
+        
               } else {
                 echo "Error: " . $sql . "<br>" . mysqli_error($con);
               } 
