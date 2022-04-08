2ed10081e9ab0ecb73b55692f7ed83adf25af1c7
Rindel Subaan <valsubaan@yahoo.com>
2022-04-09 00:20:27 +0800

    quickfix: login validation for customer

--- User/login.php
+++ User/login.php
@@ -73,22 +73,20 @@
   $user_id = "";
 
   if($results != "") {
-    while($row = mysqli_fetch_assoc($results)) {
-      $table_users = $row["customer_username"];
-      $table_passwd = $row["customer_password"];
-      $user_id = $row["customer_id"];
-    }
-    if(($username == $table_users) && ($password == $table_passwd)) {
-      $_SESSION['loggedin'] = true;
-      $_SESSION['user'] = $username;
-      $_SESSION['userID'] = $user_id;
-      header("location: index.php");
-    } else {
-      Print '<script>alert("Incorrect Password!");</script>';
-      '<script>window.location.assign("login_page.php");</script>';
-    }
+  while($row = mysqli_fetch_assoc($results)) {
+    $table_users = $row["customer_username"];
+    $table_passwd = $row["customer_password"];
+    $user_id = $row["customer_id"];
+  }
+
+  if(($username == $table_users) && ($password == $table_passwd)) {
+    $_SESSION['loggedin'] = true;
+    $_SESSION['user'] = $username;
+    $_SESSION['userID'] = $user_id;
+    header("location: index.php");
   } else {
-    Print '<script>alert("Incorrect Username!");</script>';
+    Print '<script>alert("Incorrect Username or Password!");</script>';
     Print '<script>window.location.assign("login_page.php");</script>';
   }
+}
 ?>
