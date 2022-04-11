370ad3a41af82e676792cf23faa8477f4f90c336
Rindel Subaan <valsubaan@yahoo.com>
2022-04-11 16:37:02 +0800

    feature: fetch latest shop from shops table and display items from shops

--- User/index.php
+++ User/index.php
@@ -2,7 +2,14 @@
     include 'includes/main/header.php';
     include 'includes/connection/db_connection.php';
 
-    $result = mysqli_query($link, "SELECT * FROM shops");
+    // -- `shops` table --
+    //featured shops
+    $featured_shops = mysqli_query($link, "SELECT * FROM shops");
+    //new shops
+    $new_shops = mysqli_query($link, "SELECT * FROM shops ORDER BY shop_id DESC");
+
+    // -- `items` table --
+    $items_featured = mysqli_query($link, "SELECT * FROM items LIMIT 3");
 ?> 
 <main role="main" style="background-color: #F7F3F2;">
     <div class="bg-img">
@@ -47,27 +54,29 @@
                 </ul>
             </div>
             <?php
-                $row = mysqli_fetch_row($result);
+                $featured = mysqli_fetch_row($featured_shops);
+                $new = mysqli_fetch_row($new_shops);
             ?>
             <div class="card-body">
                 <div class="tab-content" id="myTabContent">
                     <div class="tab-pane fade show active" id="featured" role="tabpanel" aria-labelledby="featured-tab">
                         <!-- featured shop -->
+                        <!-- oldest sa `shops` table -->
                         <div class="card">
                             <?php
-                                if($row != null) {
-                                    echo '<a href="merchant_portal.php"><img src="data:image/jpeg;base64,'.base64_encode($row[3]).'" class="card-img-top" /></a>';
+                                if($featured != null) {
+                                    echo '<a href="merchant_portal.php"><img src="data:image/jpeg;base64,'.base64_encode($featured[3]).'" class="card-img-top" /></a>';
                                 }
                             ?>
                             <div class="card-body">
                                 <p class="card-text"><?php 
-                                    if($row != null) {
-                                        echo $row[2];
+                                    if($featured != null) {
+                                        echo $featured[2];
                                     }
                                 ?></p>
-                                <p class="card-text"><small class="text-muted"><?php 
-                                    if($row != null) {
-                                        echo $row[5];
+                                <p class="card-text"><small class="text-muted">From <?php 
+                                    if($featured != null) {
+                                        echo $featured[5];
                                     }
                                 ?></small></p>
                             </div>
@@ -75,6 +84,7 @@
                     </div>
                     <div class="tab-pane fade" id="promoted" role="tabpanel" aria-labelledby="promoted-tab">
                         <!-- promoted -->
+                        <!-- shops with most reactions are promoted -->
                         <!--
                         
                         To do here:
@@ -90,16 +100,28 @@
                     </div>
                     <div class="tab-pane fade" id="new" role="tabpanel" aria-labelledby="new-tab">
                         <!-- new shop -->
-                        <!--
-
-                            To do here:
-                            - create query to select the most recent added shop to the `shops` table
-                        -->
+                        <!-- display newest from `shops` -->
                         <div class="card">
-                            <img src="dist/img/img1.webp" alt="..." class="card-img-top">
+                            <?php
+                                if($new != null) {
+                                    echo '<a href="merchant_portal.php"><img src="data:image/jpeg;base64,'.base64_encode($new[3]).'" class="card-img-top" /></a>';
+                                } 
+                            ?>
                             <div class="card-body">
-                                <p class="card-text">New Shop name</p>
-                                <p class="card-text"><small class="text-muted">Updated 3 mins ago.</small></p>
+                                <p class="card-text">
+                                    <?php
+                                        if($new != null) {
+                                            echo $new[2];
+                                        }
+                                    ?>
+                                </p>
+                                <p class="card-text"><small class="text-muted">
+                                    Uploaded: <?php 
+                                        if($new != null) {
+                                            echo $new[9];
+                                        }
+                                    ?>
+                                </small></p>
                             </div>
                         </div>
                     </div>
@@ -108,33 +130,36 @@
         </div>
         <div class="row">
             <div class="col">
-                <h2>Recommended</h2>
+                <!-- show items by featured shop -->
+                <h2>New items from featured shop</h2>
                 <div class="row row-cols-1 row-cols-md-2 g-4">
-                    <div class="col">
-                        <div class="card">
-                            <img src="dist/img/c1.jpg" alt="C1" class="card-img-top">
-                            <div class="card-body">
-                                <p class="card-text"><small class="text-muted">By:</small></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                    <div class="card">
-                            <img src="dist/img/c2.jpg" alt="C2" class="card-img-top">
-                            <div class="card-body">
-                                <p class="card-text"><small class="text-muted">By:</small></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                    <div class="card">
-                            <img src="dist/img/c3.jpg" alt="C3" class="card-img-top">
-                            <div class="card-body">
-                                <p class="card-text"><small class="text-muted">By:</small></p>
-                            </div>
-                        </div>
-                    </div>
+                    <?php
+                        while($item = mysqli_fetch_array($items_featured)) {
+                            Print '<div class="col">
+                                        <div class="card">';
+                            echo '<a href="#"><img src="data:image/jpeg;base64,' .base64_encode($item[5]).'" class="card-img-top"></a>';
+                            Print '<div class="card-body">
+                                        <p class="card-text"><small class="text-muted">' .$item[2] . '</small></p></div></div></div>';
+                        }
+                    ?>
                 </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h2>Popular by Location</h2>
+                <!--
+
+                To do:
+                - Display shops that are most popular by location
+                - still waiting for shop_reactions function
+
+                    -->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h2>Recommended</h2>
             </div>
         </div>
     </div>
