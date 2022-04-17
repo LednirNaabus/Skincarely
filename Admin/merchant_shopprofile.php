7f41a1b51f2b3f82736208bbb2022fe14323e599
Ayesha Keith Santos <ayeshakeithsantos@gmail.com>
2022-04-17 23:06:32 +0800

    merchant edit shop
    - added modal form for edit shop
    - still have to fix update shop file so that it'll link to db

--- Admin/merchant_shopprofile.php
+++ Admin/merchant_shopprofile.php
@@ -18,6 +18,17 @@ $resultprod = mysqli_query($con, $sqlprod);
       }
 
 $shopid = $_SESSION["shopid"];
+
+unset($_SESSION['shoplogo']);
+unset($_SESSION['shopmotto']);
+unset($_SESSION['shopdescription']);
+unset($_SESSION['shopmainbranch']);
+unset($_SESSION['shopotherbranch']);
+unset($_SESSION['shoppickup']);
+unset($_SESSION['shoppayment']);
+unset($_SESSION['shopsocials']);
+unset($_SESSION['shopcontact']);
+unset($_SESSION['shopdelivery']);
 ?>
 
 <!DOCTYPE html>
@@ -41,7 +52,7 @@ $shopid = $_SESSION["shopid"];
 <div class="container" style="font-family: Poppins;">
   <h1 class class="display-1">Shop Profile</h1>
     <br>
-    <p> View and manage your shop profile. </p>          
+    <p> View and manage your shop profile.</p>          
     <br>  
     <p><button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#myModal">Edit Shop</button></p>
 </div>
@@ -52,9 +63,112 @@ $shopid = $_SESSION["shopid"];
 
 <br>
 
-<!-- edit modal -->
+<!-- Edit Shop Profile (Modal) -->
 
-<!-- dito lalabas how the shop looks like -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Edit Shop Details</h4>
+      </div>
+      <div class="modal-body">
+
+      <?php
+        include('connection.php');      
+            $sql = "SELECT * FROM shops WHERE shop_id = '$shopid';";
+            $result = mysqli_query($con, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+
+                  $_SESSION["shopname"] = $row['shop_name'];
+                  $_SESSION["shoplogo"] = $row['shop_logo'];
+                  $_SESSION["shopmotto"] = $row['shop_motto'];
+                  $_SESSION["shopdescription"] = $row['shop_description'];
+                  $_SESSION["shopmainbranch"] = $row['shop_mainbranch'];
+                  $_SESSION["shopotherbranch"] = $row['shop_otherbranch'];
+                  $_SESSION["shoppickup"] = $row['shop_pickupareas'];
+                  $_SESSION["shopdelivery"] = $row['shop_delivery'];
+                  $_SESSION["shoppayment"] = $row['shop_payment'];
+                  $_SESSION["shopsocials"] = $row['shop_socials'];
+                  $_SESSION["shopcontact"] = $row['shop_contact'];
+
+                }
+            } else {
+                echo "0 results";
+            }
+        ?>
+
+      <form action="update_shop.php" method="post" enctype="multipart/form-data">
+
+        <div class="form-group">
+          <label for="shopname">Shop Name</label>
+          <textarea class="form-control" id="shopname" name = "shopname" rows="1"><?php echo $_SESSION["shopname"]; ?></textarea>
+        </div>
+        <div class="form-group">
+          <label for="shopmotto">Shop Motto</label>
+          <textarea class="form-control" id="shopmotto" name = "shopmotto" rows="1"><?php echo $_SESSION["shopmotto"]; ?></textarea>
+        </div>
+        <div class="form-group">
+          <label for="shopdescription">Shop Description</label>
+          <textarea class="form-control" id="shopdescription" rows="3" name = "shopdescription"><?php echo $_SESSION["shopdescription"]; ?></textarea>
+        </div>
+        <div class="form-group">
+          <label for="shoplogo">Shop Logo</label>
+          <input type="file" class="form-control-file" id="shoplogo" name="shoplogo">
+        </div>
+        <div class="form-group">
+          <label for="mainbranch">Main Branch</label>
+          <textarea class="form-control" id="mainbranch" name = "mainbranch" rows="1"><?php echo $_SESSION["shopmainbranch"]; ?></textarea>
+        </div>
+        <div class="form-group">
+          <label for="otherbranch">Other Branches</label>
+          <textarea class="form-control" id="otherbranch" name = "otherbranch" rows="1"><?php echo $_SESSION["shopotherbranch"]; ?></textarea>
+        </div>
+        <div class="form-group">
+          <label for="pickup">Pickup Areas</label>
+          <textarea class="form-control" id="pickup" name = "pickup" rows="1"><?php echo $_SESSION["shoppickup"]; ?></textarea>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="delivery">Delivery Modes</label><br>
+            <textarea class="form-control" id="delivery" name = "delivery" rows="1"><?php echo $_SESSION["shopdelivery"]; ?></textarea>
+          </div>
+          <div class="form-group col-md-6">
+            <label for="payment">Payment Modes</label><br>
+            <textarea class="form-control" id="payment" name = "payment" rows="1"><?php echo $_SESSION["shoppayment"]; ?></textarea>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="landline">Landline No.</label>
+            <textarea class="form-control" id="landline" name = "landline" rows="1"><?php $string1 = $_SESSION["shopcontact"]; $landline = strtok($string1, '/'); echo $landline;  ?></textarea>
+          </div>
+          <div class="form-group col-md-6">
+            <label for="mobile">Mobile No.</label>
+            <textarea class="form-control" id="mobile" name = "mobile" rows="1"><?php $string3 = $_SESSION["shopcontact"]; $arr = explode("/", $string3, 2); $mobile = $arr[1]; echo $mobile ?></textarea>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="socials">Social Media Links</label>
+          <textarea class="form-control" id="socials" rows="3"><?php echo $_SESSION["shopsocials"]; ?></textarea>
+        </div>
+      
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="submit" class="btn btn-primary">Edit</button>
+      </div>
+      </form>
+    </div>
+
+  </div>
+</div>
+
+<!-- Merchant Shop Profile Proper -->
     
 <div class="container p-5 my-3 border rounded" style="font-family: Poppins;">
   <h1 class="text-center">
