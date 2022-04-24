fc1bd5de868baa1b2ca7c16cd9b465e751a2f37e
Rindel Subaan <valsubaan@yahoo.com>
2022-04-24 16:56:00 +0800

    features: Search shop added, Followers tab added (to fix: total follower count for shops displayed in dashboard & array index 0 display)

--- User/index.php
+++ User/index.php
@@ -13,7 +13,7 @@
                 <div class="row">
                     <form action="search.php" method="get">
                         <div class="input-group mb-3">
-                            <input type="text" name="query" id="" placeholder="Search... " class="form-control" aria-label="Search">
+                            <input type="text" name="query" id="" placeholder="Search item... " class="form-control" aria-label="Search">
                             <span class="input-group-append">
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fas fa-search"></i>
@@ -45,6 +45,9 @@
                     <li class="nav-item">
                         <a class="nav-link" id="new-tab" data-toggle="tab" href="#new" role="tab" aria-controls="new" aria-selected="false">New</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="search-tab" data-toggle="tab" href="#search-shops" role="tab" aria-controls="search" aria-selected="false">Search</a>
+                    </li>
                 </ul>
             </div>
             <?php
@@ -53,6 +56,29 @@
                 $item_query = mysqli_query($link, "SELECT * FROM items ORDER BY item_id DESC LIMIT 5");
 
                 $shop = mysqli_fetch_array($shop_query);
+
+                $get_shops_following = mysqli_query($link, "SELECT following_shops FROM customers WHERE customer_id=".$_SESSION['userID']);
+
+                $cf = mysqli_fetch_array($get_shops_following);
+
+                $following = explode(',', $cf['following_shops']);
+                $is_following = false;
+                $shop_followers = explode("|", $shop['shop_reactions']);
+
+                foreach($shop_followers as $shop_follow_count) {
+                    if($shop_follow_count == $_SESSION['userID']) {
+                        $is_following = true;
+                    }
+                }
+                // foreach($shop_followers as $shop_follow_count) {
+                //     for($i = $shop_follow_count; $i < count($shop_followers); $i++) {
+                //         if($shop_follow_count == $_SESSION['userID']) {
+                //             $is_following = true;
+                //         }
+                //     }
+                //     // unset($shop_follow_count);
+                // }
+
             ?>
             <div class="card-body">
                 <div class="tab-content" id="myTabContent">
@@ -84,9 +110,19 @@
                                     ?></small></p>
                                 </div>
                                 <div class="float-right">
-                                    <a href="#">
-                                        <i class="fa-regular fa-heart"></i>
-                                    </a>
+                                    <?php
+
+                                        $tmp = array($cf[0]);
+                                        // print_r($tmp);
+                                        // echo $tmp[0];
+                                        $test = explode(",", $tmp[0]);
+                                        // echo $test[1]; // Will print out the first shop_name in the array `following_shops` (in this case "Pink Eme")
+                                        if($is_following) {
+                                            echo '<span class="badge bg-success" style="color: #FFF;">Following</span>';
+                                        } else {
+                                            echo '<span class="badge bg-secondary" style="color: #FFF;">Not Following</span>';
+                                        }
+                                    ?>
                                 </div>
                             </div>
                         </div>
@@ -99,7 +135,7 @@
                         To do here:
                         - create query to select shop with most reactions
                         -->
-                        <div class="card">
+                        <!-- <div class="card">
                             <img src="dist/img/login-img.jpg" alt="..." class="card-img-top">
                             <div class="card-body">
                                 <p class="card-text">Promoted shop name</p>
@@ -114,6 +150,25 @@
                                         <i class="fa-regular fa-heart"></i>
                                     </a>
                                 </div>
+                            </div>
+                        </div> -->
+                        <div class="card">
+                            <?php
+                                if($shop != null) {
+                                    $shop_promoted = mysqli_query($link, "SELECT * FROM shops ORDER BY shop_reactions");
+                                    $promoted_shops = mysqli_fetch_array($shop_promoted);
+                                } else {
+                                    echo '<p class="card-text"> There is nothing yet. Why don\'t you create a <a href="../Admin/signup.php">vendor account</a> to host a shop?</p>';
+                                }
+                            ?>
+                            <div class="card-body">
+                                <p class="card-text">
+                                    <?php
+                                        if($promoted_shops != null) {
+                                            echo $promoted_shops[2];
+                                        }
+                                    ?>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -151,12 +206,38 @@
                                     </small></p>
                                 </div>
                                 <div class="float-right">
-                                    <a href="#">
-                                        <i class="fa-regular fa-heart"></i>
-                                    </a>
+                                    <?php
+
+                                        // $shop_followers = explode("|", $shop['shop_reactions']);
+
+                                        // foreach($shop_followers as $shop_follow_count) {
+                                        //     if($shop_follow_count == $_SESSION['userID']) {
+                                        //         $is_following = true;
+                                        //         echo $shop_follow_count;
+                                        //     }
+                                        // }
+
+                                        if($is_following) {
+                                            echo '<span class="badge bg-success" style="color: #FFF;">Following</span>';
+                                        } else {
+                                            echo '<span class="badge bg-secondary" style="color: #FFF;">Not Following</span>';
+                                        }
+                                    ?>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div class="tab-pane fade" id="search-shops" role="tabpanel" aria-labelledby="search-tab">
+                        <form action="search_shop.php" method="get">
+                            <div class="input-group mb-3">
+                                <input type="text" name="query" id="" placeholder="Search a Shop... " class="form-control" aria-label="Search">
+                                <span class="input-group-append">
+                                    <button type="submit" class="btn btn-primary">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -167,13 +248,12 @@
                 <h2>New items from <?php echo $shop[2]; ?> shop</h2>
                 <div class="row row-cols-1 row-cols-md-2 g-4">
                     <?php
-                    
                         if($item_query != null) {
-                            while($row = $item_query->fetch_assoc()) {
+                            while($row = mysqli_fetch_array($item_query)) {
                                 if($row["shop_id"] == $shop["shop_id"]) {
                                     Print '<div class="col">
                                                     <div class="card">';
-                                        echo '<a href="#"><img src="data:image/jpeg;base64,' .base64_encode($row["item_image"]).'" class="card-img-top"></a>';
+                                        echo '<a href="item_portal.php?item_id='.$row["item_id"].'"><img src="data:image/jpeg;base64,' .base64_encode($row["item_image"]).'" class="card-img-top"></a>';
                                         Print '<div class="card-body">
                                                     <p class="card-text"><small class="text-muted">' .$row["item_name"] . '</small></p></div></div></div>';
                                 }
@@ -194,14 +274,64 @@
         </div>
         <div class="row">
             <div class="col">
-                <h2>Popular by Location</h2>
-                <!--
+                <h2>Following</h2>
+                <?php
+                    // if($following != null) {
+                    //     foreach($following as $shops_following) {
+                    //         if($shops_following <= 3) {
+                                // echo '<div class="col">
+                                //         <div class="card">
+                                //             <div class="card-body">';
+                                // echo            '<p class="card-text">'.$shops_following.'</p>';
+                                // echo        '</div>
+                                //             <div class="card-footer">';
+                                // echo            '<p class="float-right"> ';
+                                // echo            count($shop_followers);
+                                // echo            ' Followers </p>
+                                //             </div>
+                                //         </div>
+                                //     </div>';
+                    //         }
+                    //     }
+                    // } else {
+                    //     echo "Null";
+                    // }
 
-                To do:
-                - Display shops that are most popular by location
-                - still waiting for shop_reactions function
+                    // foreach($following as $shops_following) {
+                    //     if($shops_following <= 3) {
+                            // echo '<div class="col">
+                            //             <div class="card">
+                            //                 <div class="card-body">';
+                            // echo            '<p class="card-text">'.$shops_following.'</p>';
+                            // echo        '</div>
+                            //             <div class="card-footer">';
+                            // echo            '<p class="float-right"> ';
+                            // echo            count($shop_followers);
+                            // echo            ' Followers </p>
+                            //             </div>
+                            //         </div>
+                            //     </div>';
+                    //     }
+                    // }
 
-                    -->
+                    // ISSUE: naiinclude yung ',' sa pag echo, idk how to fix
+                    for($i = 0; $i < count($following); $i++) {
+                        if($i <= 3) {
+                            echo '<div class="col">
+                                        <div class="card">
+                                            <div class="card-body">';
+                            echo            '<p class="card-text">'.$following[$i].'</p>';
+                            echo        '</div>
+                                        <div class="card-footer">';
+                            // echo            '<p class="float-right"> ';
+                            echo            count($shop_followers);
+                            echo            ' <span class="badge bg-success float-right" style="color: #FFF;">Following</span>
+                                        </div>
+                                    </div>
+                                </div>';
+                        }
+                    }
+                ?>
             </div>
         </div>
     </div>
---
+++ User/search_shop.php
@@ -0,0 +1,91 @@
+<?php
+    include 'includes/main/header.php';
+    include 'includes/connection/db_connection.php';
+?>
+<main role="main">
+    <div class="container">
+        <div class="row" style="padding-top: 2rem;">
+            <h1 class="display-4">Results</h1>
+        </div>
+        <div class="row">
+            <form action="search_shop.php" method="get">
+                <div class="input-group mb-3">
+                    <input type="text" name="query" id="" placeholder="Search..." class="form-control">
+                    <span class="input-group-append">
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </form>
+        </div>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Search Results</li>
+            </ol>
+        </nav>
+        <!-- fetch from db -->
+        <?php
+        $output = '';
+
+        if(isset($_GET['query']) && !empty($_GET['query'])) {
+            $search = $_GET['query'];
+
+            $search_query = mysqli_query($link, "SELECT * FROM shops WHERE shop_name LIKE '%$search%'");
+            $count = mysqli_num_rows($search_query);
+        } else {
+            header("location: User/index.php");
+        }
+        ?>
+        <div class="row">
+            <div class="table-responsive">
+                <!--
+                    TO DO:
+                    - table pagination
+                -->
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Shop Name</th>
+                            <th scope="col">Main Branch</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            if($count == 0) {
+                                $output = 'No search results for "<strong>'.$search.'</strong>"';
+                                echo '<tr> <td> ' . $output . '</td> </tr>';
+                            } else {
+                                while($row = mysqli_fetch_array($search_query)) {
+
+                                    $item_shop_id = $row['shop_id'];
+
+                                    $shop_tmp_query = mysqli_query($link, "SELECT * FROM shops WHERE shop_id='$item_shop_id'");
+
+                                    $count_shop = mysqli_num_rows($shop_tmp_query);
+
+                                    if($count_shop > 0) {
+                                        $shop = mysqli_fetch_array($shop_tmp_query);
+                                        $shop_id = $shop['shop_id'];
+                                        $shop_name = $shop['shop_name'];
+
+                                        if($item_shop_id != $shop_id) {
+                                            echo "Uknown error occured.";
+                                        } else {
+                                            Print "<tr>";
+                                            Print '<td><a href="merchant_portal.php?shop_id='.$row['shop_id'].'">'.$shop_name.'</a></td>';
+                                            Print '<td>'.$row['shop_mainbranch'].'</td>';
+                                            Print "<tr>";
+                                        }
+                                    }
+                                }
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include 'includes/main/footer.php'; ?>
