accf027cb5529c8b61c502ed76f2e5e7030b5823
Rindel Subaan <valsubaan@yahoo.com>
2022-05-05 09:25:59 +0800

    fix: exception handling for image upload

--- Admin/update_shop.php
+++ Admin/update_shop.php
@@ -15,15 +15,23 @@ session_start();
     $shoppayment = $_POST['payment']; 
 
     $shoplogo = addslashes(file_get_contents($_FILES['shoplogo']['tmp_name']));
+    $img = $_FILES['shoplogo']['tmp_name'];
 
     $contact = $_POST['landline'] . " / " . $_POST['mobile'];  
     $socials = $_POST['socials'];
 
-    
+    if($img != null) {
+      $img_d = addslashes(file_get_contents($img));
+      $q = "UPDATE shops SET shop_name='$shopname', shop_logo='$img_d', shop_motto='$shopmotto', shop_description='$shopdescription', shop_mainbranch='$shopmainbranch', shop_otherbranch='$shopotherbranch', shop_pickupareas='$shoppickup', shop_delivery='$shopdelivery', shop_payment='$shoppayment', shop_contact='$contact', shop_socials='$socials' WHERE shop_id='$linkvs'";
+    } else {
+      echo "<script type ='text/JavaScript'>";
+      echo "alert('Upload must be an image')";
+      echo "</script>";
+    }
 
     $linkvs = $_SESSION["shopid"];
 
-              $sql = "UPDATE shops SET shop_name='$shopname', shop_logo='$shoplogo', shop_motto='$shopmotto', shop_description='$shopdescription', shop_mainbranch='$shopmainbranch', shop_otherbranch='$shopotherbranch', shop_pickupareas='$shoppickup', shop_delivery='$shopdelivery', shop_payment='$shoppayment', shop_contact='$contact', shop_socials='$socials' WHERE shop_id='$linkvs'";
+              $sql = "UPDATE shops SET shop_name='$shopname', shop_logo='$img_d', shop_motto='$shopmotto', shop_description='$shopdescription', shop_mainbranch='$shopmainbranch', shop_otherbranch='$shopotherbranch', shop_pickupareas='$shoppickup', shop_delivery='$shopdelivery', shop_payment='$shoppayment', shop_contact='$contact', shop_socials='$socials' WHERE shop_id='$linkvs'";
 
               if (mysqli_query($con, $sql)) {
                   ob_start();
